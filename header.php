ce9a6c1f477b430865e4d234c38ae685d6669e22
S.Hakamada <sho.hkmd@gmail.com>
2022-03-11 17:59:26 +0900

    Fix:

--- header.php
+++ header.php
@@ -5,17 +5,6 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-    <!-- Web Fonts -->
-    <script>
-      (function(d) {
-        var config = {
-          kitId: 'vla1ume',
-          scriptTimeout: 3000,
-          async: true
-        },
-        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-      })(document);
-    </script>
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?> data-page-state="loading">
